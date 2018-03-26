055c864b87aa6fe0ce7b1864922f1f787e78137c
Henry <henrywwdeng@gmail.com>
2018-03-26 13:29:25 -0700

    Refactor methods from Contains into Item and Box Services

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         ConnectionService connectionService = new ConnectionService();
-        connectionService.connect("ora_c1z8", "a41584103");
+        connectionService.connect("ora_j2z8", "a31484116");
         Session session = new Session();
         CustomerService customerService = new CustomerService(connectionService.getConnection());
         MysteryBoxService mysteryBoxService = new MysteryBoxService(connectionService.getConnection());
         ItemService itemService = new ItemService(connectionService.getConnection());
-        ContainsService containsService = new ContainsService(connectionService.getConnection());
         MainFrameController mfc = new MainFrameController(customerService);
     }
 }
